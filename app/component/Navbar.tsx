fcb405ca60728320fa34035d6077c36387b9f722
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-27 13:04:00 +0200

    initial menu bar done

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { UserButton } from '@clerk/nextjs';
+import { Layers } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 import React from 'react'
@@ -23,9 +24,15 @@ export default function d({}: Props) {
     
 
     return (
-        <div className='border-b border-base-300 px-5 md:px-[10%] py-4 relative'>
-            <div>
-                <div >
+        <div className='border-b border-base-300 px-5 md:px-[5%] py-4'>
+            <div className='flex items-center justify-between'>
+                <div className='flex items-center '>
+                    <div className='bg-accent-content text-accent rounded-full p-2'>
+                        <Layers className='w-6 h-6'/>
+                    </div>
+                    <span className='ml-2 font-bold text-2xl'>c.<span className='text-accent'>Fact</span></span>
+                </div>
+                <div className='flex items-center space-x-2'>
                     {renderLinks('btn')}
                     <UserButton/>
                 </div>
