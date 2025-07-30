5ee98d72e759a5c88cdfabb24453b11c3a8bcdc4
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-30 08:59:37 +0200

    modal restructured

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
+"use client"
 import Wrapper from "./component/Wrapper";
 
 export default function Home() {
   return (
+
     <Wrapper >
-      {/* You can open the modal using document.getElementById('ID').showModal() method */}
-      <button className="btn" onClick={() => (document.getElementById('my_modal_3')as HTMLDialogElement).showModal()}>open modal</button>
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-          </form>
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click on ✕ button to close</p>
-        </div>
-      </dialog>
+      <div className="flex flex-col space-y-4">
+        <h1 className="text-lg font-bold">Mes factures</h1>
+        <button className="btn" onClick={() => (document.getElementById('my_modal_3') as HTMLDialogElement).showModal()}>open modal</button>
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            </form>
+            <h3 className="font-bold text-lg">Hello!</h3>
+            <p className="py-4">Press ESC key or click on ✕ button to close</p>
+          </div>
+        </dialog>
+      </div>
+
     </Wrapper>
   );
 }
