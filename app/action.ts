9a696d8a46de18b0cea1a9430d8a00915498f23f
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-30 19:52:25 +0200

    ts getInvoiceByEmail finished

--- app/action.ts
+++ app/action.ts
@@ -10,7 +10,7 @@ const generateUniqueId = async () => {
     while (!isUnique) {
         uniqueId = randomBytes(3).toString('hex'); // Generates a 6-character hex string
         const existingFact = await prisma.invoice.findUnique({
-            where: { id : uniqueId }
+            where: { id: uniqueId }
         });
         if (!existingFact) {
             isUnique = true;
@@ -19,41 +19,79 @@ const generateUniqueId = async () => {
     return uniqueId;
 }
 
-export async function checkAndAddUser(email:string, name:string) {
-    if(!email)return;
-    try{
-        const existingUser = await prisma.user.findUnique({where: {email: email}});
-        if(!existingUser && name) {
+export async function checkAndAddUser(email: string, name: string) {
+    if (!email) return;
+    try {
+        const existingUser = await prisma.user.findUnique({ where: { email: email } });
+        if (!existingUser && name) {
             await prisma.user.create({
-                data: {email, name}
-            });        }
-    }catch (error) {
+                data: { email, name }
+            });
+        }
+    } catch (error) {
         console.error("Error checking or adding user:", error);
     }
 }
 
-export async function createEmptyFact(email:string, name:string){
+export async function createEmptyFact(email: string, name: string) {
     try {
-        const user = await prisma.user.findUnique({where: {email: email}});
+        const user = await prisma.user.findUnique({ where: { email: email } });
         const factID = await generateUniqueId() as string;
-        if(user){
+        if (user) {
             const newFact = await prisma.invoice.create({
-            data:{
-                id: factID, 
-                name:name,
-                userId: user?.id,                
-                issuerName: "", 
-                issuerAddress: "",
-                clientName:"",
-                clientAddress:"",
-                invoiceDate:"",
-                dueDate:"",
-                vatActive : false,
-                vatRate : 20
-            }
-        });
+                data: {
+                    id: factID,
+                    name: name,
+                    userId: user?.id,
+                    issuerName: "",
+                    issuerAddress: "",
+                    clientName: "",
+                    clientAddress: "",
+                    invoiceDate: "",
+                    dueDate: "",
+                    vatActive: false,
+                    vatRate: 20
+                }
+            });
         }
     } catch (error) {
-        console.error("Error creating empty fact:", error);        
+        console.error("Error creating empty fact:", error);
     }
-} 
+}
+
+export async function getInvoiceByEmail(email: string) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email: email },
+            include: { invoices: { include: { lines: true } } }
+        });
+
+        // Status possibles : 
+        // 1. Brouion
+        // 2. En attente 
+        // 3. Payé
+        // 4. Annulé
+        // 5. Impayé
+
+        if (user) {
+            const today = new Date();
+            const updateInvoices = await Promise.all(
+                user.invoices.map(async (invoice) => {
+                    const dueDate = new Date(invoice.dueDate);
+                    if (dueDate < today && invoice.status === 2) {
+                        const updateInvoice = await prisma.invoice.update({
+                            where: { id: invoice.id },
+                            data: { status: 5 }, // Update status to "Impayé"
+                            include: { lines: true }
+                        });
+                        return updateInvoice;
+                    }
+                    return invoice;
+                })
+            );
+            return updateInvoices;
+        }
+    } catch (error) {
+        console.error("Error fetching invoices by email:", error);
+    }
+}
