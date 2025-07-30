9864e2ae8c8dfed890f8b7d71e9b8da65693a723
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-30 17:20:23 +0200

    Home ModalDialog implemnted

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,50 @@
 "use client"
 import { Layers } from "lucide-react";
 import Wrapper from "./component/Wrapper";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
 
+  const [invoiceName, setInvoiceName] = useState("");
+  const [isNameValide, setIsNameValide] = useState(true);
+  useEffect(()=>{
+    setIsNameValide(invoiceName.length <= 40);
+  },[invoiceName])
+
+  return (
     <Wrapper >
       <div className="flex flex-col space-y-4">
         <h1 className="text-lg font-bold">Mes factures</h1>
 
         <div className="grid md:grid-cols-3 gap-4">
           <div
-            className="cursor-pointer border border-accent rounded-xl flex flex-col justify-center "
+            className="cursor-pointer border border-accent rounded-xl flex flex-col justify-center items-center p-4 "
             onClick={() => (document.getElementById('my_modal_3') as HTMLDialogElement).showModal()}>
             <div className="font-bold text-accent">Créer une facture</div>
-            <div className='bg-accent-content text-accent rounded-full p-2 mt-2'>
+            <div className='bg-accent-content text-accent rounded-full p-2 m-2'>
               <Layers className='w-6 h-6' />
             </div>
           </div>
         </div>
+
         <dialog id="my_modal_3" className="modal">
           <div className="modal-box">
             <form method="dialog">
               <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
             </form>
-            <h3 className="font-bold text-lg">Hello!</h3>
-            <p className="py-4">Press ESC key or click on ✕ button to close</p>
+            <h3 className="font-bold text-lg">Nouvelle facture</h3>
+            <input 
+              type="text" 
+              placeholder="Nom de la facture (max 40 caractères)" 
+              className="input input-bordered w-full my-4"
+              value = {invoiceName}
+              onChange={(e)=>setInvoiceName(e.target.value)}
+            />
+            {!isNameValide && <p>Le nom ne peut pas dépasser 40 caractères</p>}
+            
           </div>
         </dialog>
+
       </div>
     </Wrapper>
   );
