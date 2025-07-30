476aab375e3ae49e81b561174b29f4e73e2bcb10
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-07-30 16:19:35 +0200

    title and app deciption updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Layers } from "lucide-react";
 import Wrapper from "./component/Wrapper";
 
 export default function Home() {
@@ -7,9 +8,17 @@ export default function Home() {
     <Wrapper >
       <div className="flex flex-col space-y-4">
         <h1 className="text-lg font-bold">Mes factures</h1>
+
         <div className="grid md:grid-cols-3 gap-4">
-          <div className="cursor-pointer border border-accent rounded-xl flex justify-center" onClick={() => (document.getElementById('my_modal_3') as HTMLDialogElement).showModal()}>open modal</div>
-        </div>        
+          <div
+            className="cursor-pointer border border-accent rounded-xl flex flex-col justify-center "
+            onClick={() => (document.getElementById('my_modal_3') as HTMLDialogElement).showModal()}>
+            <div className="font-bold text-accent">Créer une facture</div>
+            <div className='bg-accent-content text-accent rounded-full p-2 mt-2'>
+              <Layers className='w-6 h-6' />
+            </div>
+          </div>
+        </div>
         <dialog id="my_modal_3" className="modal">
           <div className="modal-box">
             <form method="dialog">
