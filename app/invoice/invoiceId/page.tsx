8a382cdb3f2aef211cd15c4e10f7ac4caa37e403
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-08-03 06:11:50 +0200

    fetchInvoice addes in invoice/invoiceId page

--- app/invoice/invoiceId/page.tsx
+++ app/invoice/invoiceId/page.tsx
@@ -1,6 +1,17 @@
+import { getInvoiceById } from '@/app/action';
 import React from 'react'
 
+
 const page = ({params}:{params:Promise<{invoiceId:string}>}) => {
+    const fetchInvoice = async () => {
+        try {
+            const { invoiceId } = await params;
+            const fetchedInvoice = await getInvoiceById(invoiceId);
+        } catch (error) {
+            console.error("Error fetching invoice:", error);        
+        }
+    }
+
   return (
     <div>page</div>
   )
