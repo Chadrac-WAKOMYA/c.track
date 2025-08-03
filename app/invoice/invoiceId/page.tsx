68ebb256de4d3c3bde882fb6830d409e32000016
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-08-03 06:24:43 +0200

    fetchedInvoice initialised in invoice/invoiceId page

--- app/invoice/invoiceId/page.tsx
+++ app/invoice/invoiceId/page.tsx
@@ -1,12 +1,21 @@
 import { getInvoiceById } from '@/app/action';
-import React from 'react'
-
+import { Invoice } from '@/app/types';
+import React, { useState } from 'react'
 
 const page = ({params}:{params:Promise<{invoiceId:string}>}) => {
+    const [invoice, setInvoice] = useState<Invoice | null>(null);
+    const [initialInvoice, setInitialInvoice] = useState<Invoice | null>(null);
     const fetchInvoice = async () => {
         try {
             const { invoiceId } = await params;
             const fetchedInvoice = await getInvoiceById(invoiceId);
+            if (fetchedInvoice) {
+                setInvoice(fetchedInvoice);
+                setInitialInvoice(fetchedInvoice);
+            } else {
+                setInvoice(null);
+                setInitialInvoice(null);
+            }
         } catch (error) {
             console.error("Error fetching invoice:", error);        
         }
