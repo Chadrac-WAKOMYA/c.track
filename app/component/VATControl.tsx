459cb27e0805f5a2983ed4cf17781533d76ef92e
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-08-05 05:48:55 +0200

    handleVATChange implemented in VATControl component

--- app/component/VATControl.tsx
+++ app/component/VATControl.tsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import { Invoice } from '../types';
 
-interface Props  {
-    invoice : Invoice;
+interface Props {
+    invoice: Invoice;
     setInvoice: (invoice: Invoice) => void;
 }
 
-const VATControl: React.FC<Props> = ({invoice, setInvoice}) => {
-  return (
-    // <div className='flex items-center'>
-    //     <label className="block text-sm font-bold">TVA (%)</label>
-    // </div>
-    <></>
-  )
+const VATControl: React.FC<Props> = ({ invoice, setInvoice }) => {
+
+    const handleVATChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const isChecked = e.target.checked;
+        setInvoice({
+            ...invoice,
+            vatActive: isChecked,
+            vatRate: isChecked ? invoice.vatRate : 0, // Reset VAT rate if unchecked
+        });
+    }
+
+    return (
+        <div className='flex items-center'>
+            <label className="block text-sm font-bold">TVA (%)</label>
+            <input
+                type="checkbox"
+                className='toggle toggle-sm ml-2'
+                checked={invoice.vatActive}
+                onChange={handleVATChange}
+            />
+            {invoice.vatActive && (
+                <input
+                    type='number'
+                    className='input input-sm input-bordered w-16 ml-2'
+                />
+            )}
+        </div>
+    )
 }
 
 export default VATControl
