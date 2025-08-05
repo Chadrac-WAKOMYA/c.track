9adc0fb4a829f5084ef2bcc4bd686ca83f07ea2d
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-08-05 07:37:20 +0200

    InvoiceLines component header done

--- app/component/InvoiceLines.tsx
+++ app/component/InvoiceLines.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Invoice } from '../types';
+import { Plus } from 'lucide-react';
 
 interface Props {
     invoice: Invoice;
@@ -8,7 +9,13 @@ interface Props {
 
 const InvoiceLines : React.FC<Props> = ({ invoice, setInvoice }) =>  {
   return (
-    <div>InvoiceLines</div>
+    <div className='h-fit bg-base-200 p-5 rounded-xl w-full md:ml-4'>
+        <div className='flex justify-between items-center mb-4'>
+            <h2 className='badge badge-accent'>Produits / Services</h2>
+            <button className='btn btn-sm btn-accent rounded-xl'><Plus className='w-4'/></button>
+        </div>
+
+    </div>
   )
 }
 
--- app/invoice/[invoiceId]/page.tsx
+++ app/invoice/[invoiceId]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { getInvoiceById } from '@/app/action';
 import InvoiceInfo from '@/app/component/InvoiceInfo';
+import InvoiceLines from '@/app/component/InvoiceLines';
 import VATControl from '@/app/component/VATControl';
 import Wrapper from '@/app/component/Wrapper';
 import { Invoice } from '@/app/types';
@@ -54,7 +55,7 @@ const page = ({ params }: { params: Promise<{ invoiceId: string }> }) => {
           </div>
         </div>
 
-        <div>
+        <div className='flex flex-col md:flex-row w-full'>
           <div className='flex w-full md:w-1/3 flex-col'>
             <div className='mb-4 bg-base-200 rounded-xl p-5'>
               <div className='flex justify-between items-center mb-4'>
@@ -65,7 +66,7 @@ const page = ({ params }: { params: Promise<{ invoiceId: string }> }) => {
             <InvoiceInfo invoice={invoice} setInvoice={setInvoice}/>
           </div>
           <div className='flex w-full md:w-2/3 flex-col'>
-            
+            <InvoiceLines invoice={invoice} setInvoice={setInitialInvoice}/>
           </div>
         </div>
       </div>
